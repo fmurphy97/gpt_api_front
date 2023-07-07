954a44655a3a40ce6c667a10e1e120b11fa8dce3
fmurphy97 <federicoianmurphy@gmail.com>
2023-07-07 09:56:11 -0300

    Add image connection file

---
+++ image_connections.py
@@ -0,0 +1,38 @@
+import openai
+from IPython.display import Image, display
+import requests
+
+with open('my apikey.txt', 'r') as file:
+    openai.api_key = file.read()
+
+
+def generate_images(prompt_message, num_images=4, img_size='256x256'):
+    """
+    Generates images based on an input text
+    num_images: number of images we want to generate
+    img_size: size of the output images, must be on of ['256x256', '512x512', '1024x1024']
+    prompt_message: based on which text we want to generate images
+    """
+    return openai.Image.create(prompt=prompt_message, n=num_images, size=img_size)
+
+
+def plot_images(response):
+    """Based on a response plots images"""
+    for i, resp_i in enumerate(response['data']):
+        print(f"This is img #{i}")
+        image_url = resp_i['url']
+        display(Image(url=image_url))
+
+
+def generate_image_variations(response, resp_id, num_images=4, img_size='256x256'):
+    """
+    Generates images based on an input image
+    num_images: number of images we want to generate
+    img_size: size of the output images, must be on of ['256x256', '512x512', '1024x1024']
+    prompt_message: based on which text we want to generate images
+    """
+    return openai.Image.create_variation(
+        image=requests.get(response['data'][resp_id]["url"]).content,
+        n=num_images,
+        size=img_size
+    )
