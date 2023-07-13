575ba2a007e2fb247389794785858d7c90d2089e
fmurphy97 <federicoianmurphy@gmail.com>
2023-07-13 18:40:09 -0300

    ALlow user to re generate images

--- frontend.py
+++ frontend.py
@@ -13,8 +13,11 @@ class FrontEnd:
         with open('my apikey.txt', 'r') as file:
             self.api_key = file.read()
 
-        self.num_image = 1
-        self.selected_definition = "256x256"
+        self.images_to_gen_count = 1
+        self.selected_resolution = "256x256"
+
+        self.last_api_response = None
+        self.generated_images = []
 
     def main_page(self):
 
@@ -28,7 +31,7 @@ class FrontEnd:
         # context = connection_file.read_text_file("product_context.txt")
         context = ""
 
-        if st.button("ASK  ▶"):
+        if st.button("ASK  ▶", key="button to ask question"):
             answer = text_connection.ask_question(
                 context,
                 new_question,
@@ -40,29 +43,33 @@ class FrontEnd:
     def img_page(self):
 
         new_question = st.text_input(label="Enter your prompt here")
-        self.num_image = st.slider('Select a number', 1, 10)
+        self.images_to_gen_count = st.slider('Select a number', 1, 10)
 
         # Define the available definitions
         definitions = ["256x256", "512x512", "1024x1024"]
         # Create the dropdown
-        self.selected_definition = st.selectbox("Select Definition:", definitions)
+        self.selected_resolution = st.selectbox("Select Definition:", definitions)
 
-        if st.button("Create Images"):
-            api_response = image_connections.generate_images(prompt_message=new_question,
-                                                             num_images=self.num_image,
-                                                             img_size=self.selected_definition)
+        if st.button("CREATE ▶", key="button to create images"):
+            self.last_api_response = image_connections.generate_images(
+                prompt_message=new_question, num_images=self.images_to_gen_count, img_size=self.selected_resolution)
+            self.generated_images = image_connections.get_images(self.last_api_response)
 
-            self.display_imgs_with_regen_button(api_response)
+        self.display_imgs_with_regen_button()
 
-    def display_imgs_with_regen_button(self, api_response):
-        imgs = image_connections.get_images(api_response)
+    def display_imgs_with_regen_button(self):
+        total_images = len(self.generated_images)
+        if total_images > 0:
+            cols = st.columns(total_images)
+            for i, img in enumerate(self.generated_images):
+                cols[i].image(img)
+                cols[i].button(label=f"🔁", on_click=self.create_variations, kwargs={"img_id": i}, key=f"Regen {i}")
 
-        cols = st.columns(len(imgs))
-        for i, img in enumerate(imgs):
-            cols[i].image(img)
-            cols[i].button(label=f"variar {i}", on_click=image_connections.generate_image_variations, kwargs=
-            dict(response=api_response, resp_id=i, num_images=self.num_image, img_size=self.selected_definition))
-            # TODO: images dont display after this
+    def create_variations(self, img_id):
+        self.last_api_response = image_connections.generate_image_variations(
+            response=self.last_api_response, resp_id=img_id, num_images=self.images_to_gen_count,
+            img_size=self.selected_resolution)
+        self.generated_images = image_connections.get_images(self.last_api_response)
 
     def run(self):
         tabs = st.tabs(["Text", "Image"])
